72512d391496b46f3667b6cfe99c934ea9f1623f
land-pack <landpack@sina.com>
2019-11-27 12:04:24 +1100

    add log

--- application.py
+++ application.py
@@ -2,9 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+print("landpack-")
+
 @app.route("/")
 def index():
     return "Hello Edith"
 
 if __name__ == '__main__':
+    print("landpack-listen")
     app.run(host="0.0.0.0", port=8080)
