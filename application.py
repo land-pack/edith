3b33da1d58b81ca3b771e12ff873a8dfae19f2b0
land-pack <landpack@sina.com>
2019-11-27 12:16:11 +1100

    add conf

--- application.py
+++ application.py
@@ -1,13 +1,14 @@
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
+application.config['SERVER_NAME']= '0.0.0.0:8080'
 
 print("landpack-")
 
-@app.route("/")
+@application.route("/")
 def index():
     return "Hello Edith"
 
 if __name__ == '__main__':
     print("landpack-listen")
-    app.run(host="0.0.0.0", port=8080)
+    application.run()
