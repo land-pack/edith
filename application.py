202530b55ded15b08f7b0da4a0029457370604c8
land-pack <landpack@sina.com>
2019-11-27 13:08:42 +1100

    add

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 application = Flask(__name__)
-application.config['SERVER_NAME']= '0.0.0.0:8080'
+#application.config['SERVER_NAME']= '0.0.0.0:8080'
 
 print("landpack-")
 
